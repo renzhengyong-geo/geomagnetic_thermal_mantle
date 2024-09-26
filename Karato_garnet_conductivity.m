ab6b3de7cafda216e7c8ce891a10e71850a0c3e0
Zhengyong Ren <41980386+renzhengyong-geo@users.noreply.github.com>
2024-09-26 15:36:44 +0800

    Add files via upload

---
+++ Karato_garnet_conductivity.m
@@ -0,0 +1,8 @@
+%% Conductivity of an aggregate in Earth's internior 
+%% At different depth, the 
+Given the mineralogy and element partitioning for each mineral, and temperature
+(pressure) and oxygen fugacity, one can calculate electrical conductivity of each mineral,
+and then using a model for a conductivity of an aggregate, one can calculate the
+aggregate conductivity. Several models have been proposed to calculate the aggregate
+conductivity (McLachlan et al., 1990), but here I use the Hashin-Shtrikman average. The
+upper bound by this method is given by
